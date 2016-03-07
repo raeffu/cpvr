38fbb3cd83f7337bdaf80e6a1b1a362238a8f144
raeffu <raeffu@raeffu.ch>
2016-03-07 13:32:24 +0100

    simple debayering algorithm (RGB)

---
+++ src/Billard_Tracker.java
@@ -0,0 +1,118 @@
+import ij.ImagePlus;
+import ij.gui.NewImage;
+import ij.plugin.PNG_Writer;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+import ij.process.ImageStatistics;
+
+public class Billard_Tracker implements PlugInFilter
+{
+    private static final String IMAGE_PATH = "images/";
+
+    @Override
+    public int setup(String arg, ImagePlus imp)
+    {   return DOES_8G;
+    }
+
+    @Override
+    public void run(ImageProcessor ip1)
+    {   int w1 = ip1.getWidth();
+        int h1 = ip1.getHeight();
+        byte[] pix1 = (byte[]) ip1.getPixels();
+
+        ImagePlus imgGray = NewImage.createByteImage("GrayDeBayered", w1/2, h1/2, 1, NewImage.FILL_BLACK);
+        ImageProcessor ipGray = imgGray.getProcessor();
+        byte[] pixGray = (byte[]) ipGray.getPixels();
+        int w2 = ipGray.getWidth();
+        int h2 = ipGray.getHeight();
+
+        ImagePlus imgRGB = NewImage.createRGBImage("RGBDeBayered", w1, h1, 1, NewImage.FILL_BLACK);
+        ImageProcessor ipRGB = imgRGB.getProcessor();
+        int[] pixRGB = (int[]) ipRGB.getPixels();
+
+        long msStart = System.currentTimeMillis();
+
+        ImagePlus imgHue = NewImage.createByteImage("Hue", w1/2, h1/2, 1, NewImage.FILL_BLACK);
+        ImageProcessor ipHue = imgHue.getProcessor();
+        byte[] pixHue = (byte[]) ipHue.getPixels();
+
+        int i1 = 0, i2 = 0;
+        boolean lastRow = false;
+
+        for (int y=0; y < h1; y++)
+        {
+            for (int x=0; x < w1; x++)
+            {
+                int green, green1, green2, red, blue;
+                i1 = y * w1 + x;
+
+                if (i1 +w1 +1 >= pix1.length) {
+                    lastRow = true;
+                    break;
+                }
+
+                if (y % 2 == 0 && x % 2 == 0) {
+                    green1 = pix1[i1] & 0xff;
+                    blue = pix1[i1+1] & 0xff;
+                    red = pix1[i1+w1] & 0xff;
+                    green2 = pix1[i1+w1+1] & 0xff;
+                }
+                else if (y % 2 == 0 && x % 2 == 1) {
+                    blue = pix1[i1] & 0xff;
+                    green1 = pix1[i1+1] & 0xff;
+                    green2 = pix1[i1+w1] & 0xff;
+                    red = pix1[i1+w1+1] & 0xff;
+                }
+                else if (y % 2 == 1 && x % 2 == 0) {
+                    red = pix1[i1] & 0xff;
+                    green1 = pix1[i1+1] & 0xff;
+                    green2 = pix1[i1+w1] & 0xff;
+                    blue = pix1[i1+w1+1] & 0xff;
+                }
+                else {
+                    green1 = pix1[i1] & 0xff;
+                    red = pix1[i1+1] & 0xff;
+                    blue = pix1[i1+w1] & 0xff;
+                    green2 = pix1[i1+w1+1] & 0xff;
+                }
+                green = (green1 + green2) >> 1;
+
+                pixRGB[i1] = ((red & 0xff) << 16)+((green & 0xff) << 8) + (blue & 0xff);
+            }
+            if (lastRow) break;
+        }
+
+
+        long ms = System.currentTimeMillis() - msStart;
+        System.out.println(ms);
+        ImageStatistics stats = ipGray.getStatistics();
+        System.out.println("Mean:" + stats.mean);
+
+        PNG_Writer png = new PNG_Writer();
+        try
+        {   png.writeImage(imgRGB , IMAGE_PATH + "Billard1024x544x3.png",  0);
+//            png.writeImage(imgHue,  IMAGE_PATH + "Billard1024x544x1H.png", 0);
+//            png.writeImage(imgGray, IMAGE_PATH + "Billard1024x544x1B.png", 0);
+
+        } catch (Exception e)
+        {   e.printStackTrace();
+        }
+
+//        imgGray.show();
+//        imgGray.updateAndDraw();
+        imgRGB.show();
+        imgRGB.updateAndDraw();
+//        imgHue.show();
+//        imgHue.updateAndDraw();
+    }
+
+    public static void main(String[] args)
+    {
+        Billard_Tracker plugin = new Billard_Tracker();
+
+        ImagePlus im = new ImagePlus(IMAGE_PATH + "Billard2048x1088x1.png");
+//        im.show();
+        plugin.setup("", im);
+        plugin.run(im.getProcessor());
+    }
+}
